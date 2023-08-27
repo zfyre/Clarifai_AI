7e28da60fa050040f58d334ef3ddcff26e925078
Kshitiz <kshitiz4kaushik@gmail.com>
2023-08-28 00:55:09 +0530

    feat: added cache clearer

--- streamlit.py
+++ streamlit.py
@@ -5,6 +5,10 @@ from CodeGPT import GPT_run
 
 st.title('Code-GPT 🚀')
 
+if st.button("Clear All"):
+    # Clears all st.cache_resource caches:
+    st.cache_resource.clear()
+
 user_input = st.text_input(':violet[How can I help You?]', value="", max_chars=None, key=None, type="default", help=None, autocomplete=None, on_change=None, args=None, kwargs=None, placeholder="What was the code of segment Trees?", disabled=False, label_visibility="visible")
 
 def search():
@@ -15,4 +19,7 @@ def search():
     # st.code(out.expected_output); st.divider()
 
 st.button('search',on_click=search())
+
+
+
 
